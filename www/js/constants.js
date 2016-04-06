81cd837c3c8afa0abd5964b374584d2e95a140c5
Jojo <johannes.koelbl@web.de>
2016-04-06 13:54:04 +0200

    Change api

--- www/js/constants.js
+++ www/js/constants.js
@@ -4,6 +4,6 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter.constants', []).constant('apiUrl', 'http://localhost:3000');
+//angular.module('starter.constants', []).constant('apiUrl', 'http://localhost:3000');
 //angular.module('starter.constants', []).constant('apiUrl', 'https://jojo.fachschaft.in');
-//angular.module('starter.constants', []).constant('apiUrl', 'https://seesight.herokuapp.com');
+angular.module('starter.constants', []).constant('apiUrl', 'https://seesight.herokuapp.com');
